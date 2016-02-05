68c27835bb465f5a00a0ae303c5d1cb78308b3a5
Luke LaFountain <ldlafountain@oakland.edu>
2016-02-04 20:33:45 -0500

    Added method call for the For Loop. Tested. Application Complete.

--- BeerSong.java
+++ BeerSong.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-		
+		Ninety_Nine_Bottles_of_Beer();
     }
 
  public static void Ninety_Nine_Bottles_of_Beer(){
