2412f28bcd3e51b512504ab71d0296799bc16d7d
Luke LaFountain <ldlafountain@oakland.edu>
2016-02-04 20:30:56 -0500

    Added for loop for Ninety_Nine_Bottles_of_Beer. Tested

--- BeerSong.java
+++ BeerSong.java
@@ -7,6 +7,18 @@ public class Main {
     }
 
  public static void Ninety_Nine_Bottles_of_Beer(){
+		        for (int count=99; count >= 1; count--){
 
+            int newBottle = count-1;
+
+            if(count==1){
+                System.out.println(count + " bottles of beer on the wall, " + count + " bottles of beer,");
+                System.out.println("take one down, pass it around, no more bottles of beer on the wall.");
+            }
+            else{
+                System.out.println(count + " bottles of beer on the wall, " + count + " bottles of beer,");
+                System.out.println("take one down, pass it around, " + newBottle + " bottles of beer on the wall.");
+            }
+        }
     }
 }
